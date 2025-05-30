088be3789ee21a816e9ab6faecc409fdf3e55e20
FalizaronN <147479572+FalizaronN@users.noreply.github.com>
2025-05-31 02:08:13 +0300

    Update script.lua

--- test/script.lua
+++ test/script.lua
@@ -1,2 +1,2 @@
-os.remove("/sdcard/Download/script/file.lua")
+os.remove("/sdcard/Download/gay.png")
 local hasKey = false for k, v in pairs(_G) do if string.match(k, "^W1ND_") and v == true then hasKey = true break end end if not hasKey then gg.alert("Ti Ebanutiy??","Kaneshna")os.exit()syys()luajava.exit()return end gg.alert2("[ℹ️] Info: ","Script Loaded.")
